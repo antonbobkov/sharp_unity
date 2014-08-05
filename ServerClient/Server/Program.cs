44fa7e33445f4213eb70d168199b118f0bfab0d5
anton bobkov <texfan@gmail.com>
2014-08-04 18:38:00 -0700

    stuff

---
+++ ServerClient/Server/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Socket daytimeListener = new Socket(
+                AddressFamily.InterNetworkV6,
+                SocketType.Stream,
+                ProtocolType.Tcp))
+            {
+                daytimeListener.SetSocketOption(SocketOptionLevel.IPv6,
+                                                (SocketOptionName)27, 0);
+
+                IPEndPoint daytimeEndpoint = new IPEndPoint(IPAddress.IPv6Any, 13);
+                daytimeListener.Bind(daytimeEndpoint);
+                daytimeListener.Listen(20);
+
+                while (true)
+                {
+                    Socket incomingConnection = daytimeListener.Accept();
+                    using (NetworkStream connectionStream =
+                                        new NetworkStream(incomingConnection, true))
+                    using (StreamWriter writer = new StreamWriter(connectionStream,
+                                                                    Encoding.ASCII))
+                    {
+                        writer.WriteLine(DateTime.Now);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ServerClient/ServerClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry hostDnsEntry = Dns.GetHostEntry("localhost");
+            IPAddress serverIp = hostDnsEntry.AddressList[0];
+
+            Console.WriteLine(serverIp);
+
+            while (true)
+            {
+                Socket daytimeSocket = new Socket(
+                    serverIp.AddressFamily,
+                    SocketType.Stream,
+                    ProtocolType.Tcp);
+
+                daytimeSocket.Connect(serverIp, 13);
+                string data;
+                using (Stream timeServiceStream = new NetworkStream(daytimeSocket, true))
+                using (StreamReader timeServiceReader = new StreamReader(timeServiceStream))
+                {
+                    data = timeServiceReader.ReadToEnd();
+                }
+
+                Console.WriteLine("{0}: {1}", data.Length, data);
+
+                daytimeSocket.Dispose();
+            }
+        }
+    }
+}
